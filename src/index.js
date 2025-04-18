69e3d7b760ffab430b30c5871bedf20c690da69f
Cardona1204 <manuelcardona1204@gmail.com>
2025-04-18 00:02:33 -0500

    Se implementa usuario en el index para su ejecucion

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express'
 import userRouter from './routes/user.route.js'
 import reportRoute from './routes/report.route.js'
+import usuarioRoute from './routes/usuario.route.js'
 import morgan from 'morgan';
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use('/api/users', userRouter)
 app.use('/api', reportRoute);
+app.use('/api', usuarioRoute);
 
 app.get('/', (req, res) => {
     res.send('<h1>GEOCRIMEN</h1>')
