60b65c1485bcc60a33a5c0f2b4e49cb903266904
Cardona1204 <manuelcardona1204@gmail.com>
2025-04-26 00:36:46 -0500

    Se agrega controladores para obtener ubicacion segmentada por pais, ciudad y barrio

--- src/controllers/ubicacion.controller.js
+++ src/controllers/ubicacion.controller.js
@@ -1,1 +1,48 @@
 import db from "../database/db_connect.js"
+
+export const obtenerPaises = async (req,res) => {
+    const sql = 'SELECT * FROM pais'
+    try {
+        const [rows] = await db.query(sql);
+        res.json({
+            data:rows
+        });
+    } catch (error) {
+        console.error('Se detecto un error', error.message);
+        return res.status(500).json({
+            error: error.message
+        })
+    } 
+}
+
+export const obtenerCiudadByPais = async (req,res) =>{
+    const {pais_id} =req.params;
+    const sql = 'SELECT * FROM ciudad WHERE Id_pais = ?'
+    try {
+        const [rows] = await db.query(sql, [pais_id]);
+        res.json({
+            data: rows
+        })
+    } catch (error) {
+        console.error('Error al obtener ciudades');
+        return res.status(500).json({
+            error: error.message
+        });
+    } 
+};
+
+export const obtenerBarrioByCiudad = async (req,res) =>{
+    const {ciudad_id}=req.params;
+    const sql = 'SELECT * FROM barrio where Id_ciudad = ?'
+    try {
+        const [rows] = await db.query(sql, [ciudad_id]);
+        res.json({
+            data: rows
+        })
+    } catch (error) {
+        console.log('Error al mostrar los barrios');
+        return res.status(500).json({
+            error: error.message
+        });
+    }
+};
