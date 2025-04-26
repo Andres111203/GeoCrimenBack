492e43762e1f7cccd9b6b4aece6dbc9a949b295e
Cardona1204 <manuelcardona1204@gmail.com>
2025-04-26 00:37:31 -0500

    Se agrega ruta para la conexion

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import reportRoute from './routes/report.route.js'
 import usuarioRoute from './routes/usuario.route.js'
 import morgan from 'morgan';
 import cors from 'cors';
+import extraerUbicacion from './routes/ubicacion.route.js';
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,6 +20,10 @@ app.use(express.urlencoded({extended: true}))
 app.use('/api/users', userRouter)
 app.use('/api', reportRoute);
 app.use('/api', usuarioRoute);
+
+app.use('/api', extraerUbicacion);
+// app.use('/api',extraerUbicacion);
+
 
 app.get('/', (req, res) => {
     res.send('<h1>GEOCRIMEN</h1>')
